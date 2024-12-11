003ac37fd227d3a7c8405af73b60f729efbe984d
lyaroshenko <lyaroshenko@kse.org.ua>
2024-12-11 14:58:41 +0200

    Додана перевірка унікальності слів

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
 full_list = words_fetcher.fetch_words(min_letters=3, max_letters=9)
 word = words[random.randrange(0, len(words))]
 
-print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
-print(f"Your word is '{word}'")
+print(f"Cae up {WORDS_TO_WIN} words from letters in word provided by me?")
+print(f"Your word is '{word}'hbbhecbeecf")
 
 
 while not is_game_over():
